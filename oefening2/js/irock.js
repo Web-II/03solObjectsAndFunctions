39a12aa96aae28d413ae83e9f08ed2c78515fe74
Lotte Van Steenberghe <lotte.vansteenberghe@hogent.be>
2023-02-22 10:55:08 +0100

    Add files via upload

--- oefening2/js/irock.js
+++ oefening2/js/irock.js
@@ -1,19 +1,20 @@
-'use strict';
+"use strict";
 
 const myPetRock = {
-  userName: '',
-  image: 'images/rock.png',
+  userName: "",
+  image: "images/rock.png",
   resetImage: function () {
-    this.image = 'images/rock.png';
+    this.image = "images/rock.png";
   },
 };
 
-function greetUser() {
-  window.alert('Hello, I am your pet rock.');
-};
+export function greetUser() {
+  document.getElementById("rockImg").onclick = touchRock;
+  window.alert("Hello, I am your pet rock.");
+}
 
 function showPetRock() {
-  document.getElementById('rockImg').src = myPetRock.image;
+  document.getElementById("rockImg").src = myPetRock.image;
 }
 
 function touchRock() {
@@ -21,23 +22,16 @@ function touchRock() {
     window.alert(`I like the attention, ${myPetRock.userName}. Thank you...`);
   } else {
     const userName = window.prompt(
-      'What is your name?',
-      'Enter your name here.'
+      "What is your name?",
+      "Enter your name here."
     );
     if (userName) {
       myPetRock.userName = userName;
       window.alert(`It is good to meet you, ${myPetRock.userName}!`);
     }
   }
-  myPetRock.image = 'images/rock_happy.png';
+  myPetRock.image = "images/rock_happy.png";
   showPetRock();
   myPetRock.resetImage();
   setTimeout(showPetRock, 2 * 1000);
 }
-
-function init() {
-  greetUser();
-  document.getElementById('rockImg').onclick = touchRock;
-}
-
-window.onload = init;
